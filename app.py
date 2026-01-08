ade2dcc2f3dd94bfa49546aa3ba13b509d4a6f9f
github Anggukk <goodhappy729@naver.com>
2026-01-08 13:54:30 +0900

    arduino 시리얼통신 Flask Web 연결

--- app.py
+++ app.py
@@ -1,28 +1,53 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import serial
 
-app=Falsk(__name__)
+app=Flask(__name__)
 
-ser=serial.Serial("COM3",9600,timeout=1)
+ser=None
 
 latest_data={
-    "temperature":None,
-    "humidity":None,
-    "status":None
+    "temperature":"N/A",
+    "humidity":"N/A",
+    "status":"UNKNOWN"
 }
+
+def init_serial():
+    global ser
+    try:
+        ser=serial.Serial("COM3",9600,timeout=1)
+        print("Serial connected")
+    except Exception as e:
+        print("Serial error:",e)
+        ser=None
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/data")
 def get_data():
-    if ser.in_waiting:
-        line=ser.readline().decode().strip()
+    global ser
 
-        if line!="ERROR":
-            temp,hum,status=line.split(",")
-            latest_data["temperature"]=temp
-            latest_data["humidity"]=hum
-            latest_data["status"]=status
+    if ser is None:
+        init_serial()
+
+    if ser and ser.in_waiting:
+
+        try:
+            line=ser.readline().decode().strip()
+            print("RAW",line)
+
+            if line!="ERROR":
+                temp,hum,status=line.split(",")
+                latest_data["temperature"]=temp
+                latest_data["humidity"]=hum
+                latest_data["status"]=status
+
+        except:
+            pass
+            
 
         return jsonify(latest_data)
 
-    if __name__=="__main":
-        app.run(debug=True)
+if __name__=="__main__":
+    app.run(debug=True)
