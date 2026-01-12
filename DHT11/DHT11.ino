16b7a85dd29e235b677a138979834c9d6c854cda
github Anggukk <goodhappy729@naver.com>
2026-01-12 13:55:41 +0900

    IoT 환경 시스템 시리얼 통신

--- DHT11/DHT11.ino
+++ DHT11/DHT11.ino
@@ -2,37 +2,104 @@
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
-#define LED_PIN 8
+#define LDR_PIN A0
+#define BUTTON_PIN 7
+#define BUZZER_PIN 6
+#define LED_GREEN 3
+#define LED_BLUE 4
+#define LED_RED 5
 
 DHT dht(DHTPIN,DHTTYPE);
 
-float TEMP_LIMIT=28.0;
-float HUM_LIMIT=70.0;
+float TEMP_GOOD=26.0;
+float TEMP_DANGER=30.0;
+float HUM_GOOD=60.0;
+float HUM_DANGER=75.0;
+
+int LIGHT_THRESHOULD=300;
+
+bool autoMode=true;
+bool lastButtonState=LOW;
+
+String level="GOOD";
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
-  pinMode(LED_PIN, OUTPUT);
+
+  pinMode(LDR_PIN, INPUT);
+  pinMode(BUTTON_PIN,INPUT_PULLUP);
+  pinMode(BUZZER_PIN,OUTPUT);
+
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+
+  digitalWrite(BUZZER_PIN,LOW);
 }
 
 void loop() {
   delay(2000);
 
+  bool buttonState=digitalRead(BUTTON_PIN);
+  if(buttonState==LOW && lastButtonState==HIGH){
+    autoMode=!autoMode;
+    delay(300);
+  }
+  lastButtonState=buttonState;
+
   float hum=dht.readHumidity();
   float temp=dht.readTemperature();
+  int light=analogRead(LDR_PIN);
 
   if(isnan(hum)||isnan(temp)){
     Serial.println("ERROR");
     return;
   }
 
-  bool warning =(temp>=TEMP_LIMIT || hum>=HUM_LIMIT);
+  if(temp>=TEMP_DANGER || hum>HUM_DANGER){
+    level="DANGER";
+  }
+  else if (temp>=TEMP_GOOD || hum>=HUM_GOOD) { 
+    level="CAUTION";
+  }
+  else{
+    level="GOOD";
+  }
 
-  digitalWrite(LED_PIN, warning?HIGH:LOW);
+  if(autoMode){
+    if(level=="GOOD"){
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_BLUE, LOW);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(BUZZER_PIN, LOW);
+    }
+    else if (level=="CAUTION") {
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, HIGH);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(BUZZER_PIN, LOW);
+    }
+    else if(level=="DANGER"){
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, LOW);
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(BUZZER_PIN, HIGH);
+    }
+
+    // green led를 조명으로 활용
+    if(light<LIGHT_THRESHOULD){
+      digitalWrite(LED_GREEN, HIGH);
+    }
+  }
 
   Serial.print(temp);
   Serial.print(",");
   Serial.print(hum);
   Serial.print(",");
-  Serial.println(warning?"WARNING":"NORMAL");
+  Serial.print(light);
+  Serial.print(",");
+  Serial.print(autoMode?"AUTO":"MANUAL");
+  Serial.print(",");
+  Serial.println(level);
 }
